f7c02d7fd1df522725bae323b9e3b3bf3d9ec20a
Dayan Bravo <dayan3847@gmail.com>
2023-02-10 18:58:51 -0600

    test: to continue in other computer

--- BinaryClassificationThroughLogisticRegression.py
+++ BinaryClassificationThroughLogisticRegression.py
@@ -4,17 +4,19 @@ import matplotlib.pyplot as plt
 class BinaryClassificationThroughLogisticRegression:
 
     def __init__(self):
-        self.data_point = None
+        self.data_points = None
 
     def init(self):
         self.load_data_points()
 
     # Generate Data Points
     def load_data_points(self):
-        if self.data_point is not None:
+        if self.data_points is not None:
             return
         file = open('data.txt', 'r')
-        lines = file.readlines()
+        for line in file:
+
+
         file.close()
         x1_list = []
         x2_list = []
@@ -24,23 +26,12 @@ class BinaryClassificationThroughLogisticRegression:
             (x1, x2, y) = line.split()
             x1_list.append(float(x1))
             x2_list.append(float(x2))
-            y_list.append(1 == int(y))
-        self.data_point = {
-            True: {
-                'x_list': [],
-                'y_list': [],
-            },
-            False: {
-                'x_list': [],
-                'y_list': [],
-            },
+            y_list.append(int(y))
+        self.data_points = {
+            'x_list': [],
+            'y_list': [],
+            'z_list': [],
         }
-        for i in range(len(x1_list)):
-            x1i = x1_list[i]
-            x2i = x2_list[i]
-            yi = y_list[i]
-            self.data_point[yi]['x_list'].append(x1i)
-            self.data_point[yi]['y_list'].append(x2i)
 
     def plot_data(self):
         # Data
@@ -51,10 +42,10 @@ class BinaryClassificationThroughLogisticRegression:
         plt.axvline(color='black')
         plt.axhline(color='black')
         # points
-        true_x1 = self.data_point[True]['x_list']
-        true_x2 = self.data_point[True]['y_list']
-        false_x1 = self.data_point[False]['x_list']
-        false_x2 = self.data_point[False]['y_list']
+        true_x1 = self.data_points[True]['x_list']
+        true_x2 = self.data_points[True]['y_list']
+        false_x1 = self.data_points[False]['x_list']
+        false_x2 = self.data_points[False]['y_list']
         plt.scatter(true_x1, true_x2, color='green', label='positive')
         plt.scatter(false_x1, false_x2, color='red', label='negative')
         plt.legend()
